956c73296d667ed012ad418c47ac26e3ad23a03d
Gaurav Wadhwa <gauravw@amazon.com>
2021-09-02 21:47:19 +0530

    first commit

---
+++ src/test/java/org/example/APIAutomation.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class APIAutomation {
+
+  @Test
+  public void test_sum() {
+    int x = 2;
+    int y = 2;
+    Assert.assertEquals(4, x + y);
+  }
+}
